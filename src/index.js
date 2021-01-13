5ea0fa829cf3fbe8531be6871b971a905841a7f4
Scott Bowles <shbowles@gmail.com>
2021-01-13 16:03:01 -0500

    Apply window.innerHeight vh fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './components/App';
+import { applyWindowHeightvhFix } from './utils';
+
+applyWindowHeightvhFix();
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/utils.js
+++ src/utils.js
@@ -48,3 +48,10 @@ export function useLocalStorage(key, initialValue) {
 
   return [storedValue, setValue];
 }
+
+export function applyWindowHeightvhFix() {
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  const vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
