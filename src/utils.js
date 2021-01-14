25f06bae9aa1ba46f95194dcbfe88d3e885bb487
scottBowles <shbowles@gmail.com>
2021-01-14 00:02:47 -0500

    Add event listener for vh fix

--- src/utils.js
+++ src/utils.js
@@ -54,4 +54,11 @@ export function applyWindowHeightvhFix() {
   const vh = window.innerHeight * 0.01;
   // Then we set the value in the --vh custom property to the root of the document
   document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+  // We listen to the resize event
+  window.addEventListener('resize', () => {
+    // We execute the same script as before
+    const newvh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${newvh}px`);
+  });
 }
